9a8a884e677d6084e6db08c1a1915090cdbff21e
MarcoPoloPenaRivera <A01754314@tec.mx>
2026-04-19 01:28:49 -0600

    TODOS LSO TEXTYPING HECHOS
    Se terminaron todas las escenas variantes de TextTyping teniendo una de suma/resta, una de multi/division, una de sucesiones numericas, una de patrones numericos, una de mayor o menor que,  y una ultima de suma/resta/multi/division, todas funcionales.

--- Assets/Scripts/ScriptsTextTyping 2/BancoPreguntas/BancoPreguntas.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using Unity.Multiplayer.Center.Common;
-using UnityEngine;
-
-public class BancoPreguntas : MonoBehaviour
-{
-    [SerializeField]
-    private List<Pregunta> m_questionlist = null;
-}
--- Assets/Scripts/ScriptsTextTyping/GameManager.cs
+++ Assets/Scripts/ScriptsTextTyping/GameManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
 
 public class MathGameManager : MonoBehaviour
 {
@@ -18,9 +20,23 @@ public class MathGameManager : MonoBehaviour
 
     private int correctAnswer;
 
+    public enum GameMode
+    {
+        SumaResta,
+        MultDiv,
+        Patrones,
+        Sucesiones,
+        MayorMenor,
+        Mixto
+    }
+    private GameMode gameMode;
     void Start()
     {
+        Debug.Log("ESCENA ACTUAL: " + SceneManager.GetActiveScene().name);
         Debug.Log("INICIANDO JUEGO");
+
+        SetGameModeByScene();
+
         GenerateQuestion();
     }
 
@@ -39,57 +55,196 @@ public class MathGameManager : MonoBehaviour
     }
 
     void GenerateQuestion()
+{
+    switch (gameMode)
     {
-        Debug.Log("=== GENERANDO NUEVA PREGUNTA ===");
+        case GameMode.SumaResta:
+            GenerateSumaResta();
+            break;
 
-        int num1 = Random.Range(10, 100);
-        int num2 = Random.Range(10, 100);
+        case GameMode.MultDiv:
+            GenerateMultDiv();
+            break;
 
-        bool isAddition = Random.value > 0.5f;
+        case GameMode.Patrones:
+            GeneratePatrones();
+            break;
 
-        if (isAddition)
-        {
-            correctAnswer = num1 + num2;
-            questionText.text = num1 + " + " + num2;
-        }
-        else
-        {
-            if (num2 > num1)
-            {
-                int temp = num1;
-                num1 = num2;
-                num2 = temp;
-            }
+        case GameMode.Sucesiones:
+            GenerateSucesiones();
+            break;
 
-            correctAnswer = num1 - num2;
-            questionText.text = num1 + " - " + num2;
-        }
+        case GameMode.MayorMenor:
+            GenerateMayorMenor();
+            break;
 
-        Debug.Log("Pregunta: " + questionText.text);
-        Debug.Log("Respuesta correcta: " + correctAnswer);
-
-        List<int> answers = GenerateAnswers(correctAnswer);
-
-        for (int i = 0; i < answerButtons.Length; i++)
-        {
-            int capturedAnswer = answers[i];
-            int index = i; 
-
-            if (i < answerTexts.Length)
-            {
-                answerTexts[i].text = capturedAnswer.ToString();
-            }
-            else
-            {
-                Debug.LogError("Falta asignar answerTexts en el índice: " + i);
-            }
-
-            Debug.Log("Botón " + i + ": " + capturedAnswer);
-
-            answerButtons[i].onClick.RemoveAllListeners();
-            answerButtons[i].onClick.AddListener(() => SelectAnswer(capturedAnswer, index));
-        }
+        case GameMode.Mixto:
+            GenerateMixto();
+            break;
     }
+    Debug.Log("USANDO MODO: " + gameMode);
+}
+
+void GenerateSumaResta()
+{
+    int num1 = Random.Range(10, 100);
+    int num2 = Random.Range(10, 100);
+
+    bool isAddition = Random.value > 0.5f;
+
+    if (isAddition)
+    {
+        correctAnswer = num1 + num2;
+        questionText.text = num1 + " + " + num2;
+    }
+    else
+    {
+        if (num2 > num1)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        correctAnswer = num1 - num2;
+        questionText.text = num1 + " - " + num2;
+    }
+
+    SetupAnswers(correctAnswer);
+}
+
+void GenerateMultDiv()
+{
+    int num1 = Random.Range(2, 20);
+    int num2 = Random.Range(2, 20);
+
+    bool isMult = Random.value > 0.5f;
+
+    if (isMult)
+    {
+        correctAnswer = num1 * num2;
+        questionText.text = num1 + " × " + num2;
+    }
+    else
+    {
+        correctAnswer = num1;
+        int result = num1 * num2;
+
+        questionText.text = result + " ÷ " + num2;
+    }
+
+    SetupAnswers(correctAnswer);
+}
+
+void GeneratePatrones()
+{
+    int start = Random.Range(1, 20);
+    int step = Random.Range(2, 10);
+
+    int a = start;
+    int b = a + step;
+    int c = b + step;
+    int d = c + step;
+
+    correctAnswer = d + step;
+
+    questionText.text = a + ", " + b + ", " + c + ", " + d + ", ?";
+
+    SetupAnswers(correctAnswer);
+}
+
+void GenerateSucesiones()
+{
+    int a = Random.Range(1, 10);
+    int b = Random.Range(1, 10);
+
+    int c = a + b;
+    int d = b + c;
+
+    correctAnswer = c + d;
+
+    questionText.text = a + ", " + b + ", " + c + ", " + d + ", ?";
+
+    SetupAnswers(correctAnswer);
+}
+
+void GenerateMayorMenor()
+{
+    int a = Random.Range(10, 100);
+    int b = Random.Range(10, 100);
+
+    bool askGreater = Random.value > 0.5f;
+
+    if (askGreater)
+    {
+        correctAnswer = Mathf.Max(a, b);
+        questionText.text = "¿Cuál es MAYOR?\n" + a + " o " + b;
+    }
+    else
+    {
+        correctAnswer = Mathf.Min(a, b);
+        questionText.text = "¿Cuál es MENOR?\n" + a + " o " + b;
+    }
+
+    // Generar respuestas más lógicas
+    List<int> answers = new List<int>();
+    answers.Add(a);
+    answers.Add(b);
+
+    while (answers.Count < 8)
+    {
+        int fake = Random.Range(10, 100);
+        if (!answers.Contains(fake))
+            answers.Add(fake);
+    }
+
+    ShuffleAndAssign(answers);
+}
+
+void GenerateMixto()
+{
+    if (Random.value > 0.5f)
+        GenerateSumaResta();
+    else
+        GenerateMultDiv();
+}
+
+void SetupAnswers(int correct)
+{
+    List<int> answers = GenerateAnswers(correct);
+
+    for (int i = 0; i < answerButtons.Length; i++)
+    {
+        int capturedAnswer = answers[i];
+        int index = i;
+
+        answerTexts[i].text = capturedAnswer.ToString();
+
+        answerButtons[i].onClick.RemoveAllListeners();
+        answerButtons[i].onClick.AddListener(() => SelectAnswer(capturedAnswer, index));
+    }
+}
+void ShuffleAndAssign(List<int> answers)
+{
+    for (int i = 0; i < answers.Count; i++)
+    {
+        int randomIndex = Random.Range(i, answers.Count);
+        int temp = answers[i];
+        answers[i] = answers[randomIndex];
+        answers[randomIndex] = temp;
+    }
+
+    for (int i = 0; i < answerButtons.Length; i++)
+    {
+        int capturedAnswer = answers[i];
+        int index = i;
+
+        answerTexts[i].text = capturedAnswer.ToString();
+
+        answerButtons[i].onClick.RemoveAllListeners();
+        answerButtons[i].onClick.AddListener(() => SelectAnswer(capturedAnswer, index));
+    }
+}
 
     List<int> GenerateAnswers(int correct)
     {
@@ -164,4 +319,44 @@ public class MathGameManager : MonoBehaviour
             btn.interactable = false;
         }
     }
+
+    void SetGameModeByScene()
+{
+    string sceneName = SceneManager.GetActiveScene().name;
+
+    switch (sceneName)
+    {
+        case "TextTyping":
+            gameMode = GameMode.SumaResta;
+            break;
+
+        case "TextTyping2":
+            gameMode = GameMode.MultDiv;
+            break;
+
+        case "TextTyping3":
+            gameMode = GameMode.Patrones;
+            break;
+
+        case "TextTyping4":
+            gameMode = GameMode.Sucesiones;
+            break;
+
+        case "TextTyping5":
+            gameMode = GameMode.MayorMenor;
+            break;
+
+        case "TextTyping6":
+            gameMode = GameMode.Mixto;
+            break;
+
+        default:
+            Debug.LogWarning("Escena no reconocida, usando SumaResta por defecto");
+            gameMode = GameMode.SumaResta;
+            break;
+    }
+
+    Debug.Log("Modo de juego: " + gameMode);
+}
+
 }
