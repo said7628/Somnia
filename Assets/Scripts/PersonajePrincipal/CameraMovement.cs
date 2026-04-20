43e529e0a7f4e78d2a14ab19c54da8294bd46bd0
SaidValseca <saidvalseca@gmail.com>
2026-04-20 00:48:02 -0600

    MArcoM

--- Assets/Scripts/PersonajePrincipal/CameraMovement.cs
+++ Assets/Scripts/PersonajePrincipal/CameraMovement.cs
@@ -4,24 +4,28 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float smoothSpeed = 5f;
-    [SerializeField] private Vector3 offset;
+
+    private float fixedZ;
+
+    void Start()
+    {
+        fixedZ = transform.position.z;
+    }
 
     void LateUpdate()
     {
         if (target == null) return;
 
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = new Vector3(
+            target.position.x,
+            target.position.y,
+            fixedZ
+        );
 
-        Debug.Log("Target pos: " + target.position);
-        Debug.Log("Offset: " + offset);
-        Debug.Log("Desired pos: " + desiredPosition);
-
-        Vector3 smoothedPosition = Vector3.Lerp(
+        transform.position = Vector3.Lerp(
             transform.position,
             desiredPosition,
             smoothSpeed * Time.deltaTime
         );
-
-        transform.position = smoothedPosition;
     }
 }
