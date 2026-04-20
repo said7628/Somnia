208d4522a6376c465aa08ebde3e6f866b8205161
liaaansito <A01803416@tec.mx>
2026-04-19 20:15:43 -0600

    JIJIJA la salvamos chavos

---
+++ Assets/Scripts/Islas/Memoria_Islas.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Memoria_Islas : MonoBehaviour
+{
+    // El post-it para saber de que isla venimos
+    public static string islaDeDondeVengo = "Isla1"; 
+
+    // El post-it para nuestra monedas que tenemos hipoteticamente hasta que se conecte la BD (Le ponemos 200 de prueba)
+    public static int misMonedas = 200; 
+}
