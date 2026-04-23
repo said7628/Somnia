29faa0cac170abcd88a33d3077a2f6f13360315f
MarcoPoloPenaRivera <A01754314@tec.mx>
2026-04-22 20:45:59 -0600

    FINALMENTE QUEDO EL PERSONAJE PRINCIPAL CON SUS ANIMACIONES DE IDLE
    Ya quedaron las animaciones de idle del personaje principal con todo y su sistema de personalización.

--- Assets/Scripts/PersonajePrincipal/PlayerVisual.cs
+++ Assets/Scripts/PersonajePrincipal/PlayerVisual.cs
@@ -4,14 +4,17 @@ public class PlayerVisual : MonoBehaviour
 {
     [Header("Animators")]
     [SerializeField] private Animator faceAnimator;
-    [SerializeField] private Animator eyesAnimator;
     [SerializeField] private Animator outfitAnimator;
 
-    [Header("Animator Controllers")]
-    [SerializeField] private RuntimeAnimatorController[] faceAnimators;
-    [SerializeField] private RuntimeAnimatorController[] eyesAnimators;
-    [SerializeField] private RuntimeAnimatorController[] eyesWhiteAnimators;
-    [SerializeField] private RuntimeAnimatorController[] outfitAnimators;
+    [Header("Eyes (Separados)")]
+    [SerializeField] private Animator eyesNormalAnimator;
+    [SerializeField] private Animator eyesWhiteAnimator;
+
+    [Header("Animation Clips")]
+    [SerializeField] private AnimationClip[] faceClips;
+    [SerializeField] private AnimationClip[] outfitClips;
+    [SerializeField] private AnimationClip[] eyesClips;
+    [SerializeField] private AnimationClip[] eyesWhiteClips;
 
     private PlayerCustomizationManager data;
 
@@ -29,20 +32,49 @@ public class PlayerVisual : MonoBehaviour
             return;
         }
 
+        // 🔒 PROTECCIÓN DE ÍNDICES
+        if (data.selectedFaceColor >= faceClips.Length ||
+            data.selectedOutfit >= outfitClips.Length ||
+            data.selectedEyes >= eyesClips.Length)
+        {
+            Debug.LogError("Índice fuera de rango en PlayerVisual");
+            return;
+        }
+
         // FACE
-        faceAnimator.runtimeAnimatorController = faceAnimators[data.selectedFaceColor];
+        faceAnimator.Play(faceClips[data.selectedFaceColor].name, 0, 0f);
 
         // OUTFIT
-        outfitAnimator.runtimeAnimatorController = outfitAnimators[data.selectedOutfit];
+        outfitAnimator.Play(outfitClips[data.selectedOutfit].name, 0, 0f);
 
-        // EYES
-        if (data.selectedFaceColor == 9)
+        // 🔥 EYES (SOLUCIÓN DEFINITIVA)
+
+        // Apagar ambos primero
+        if (eyesNormalAnimator != null)
+            eyesNormalAnimator.gameObject.SetActive(false);
+
+        if (eyesWhiteAnimator != null)
+            eyesWhiteAnimator.gameObject.SetActive(false);
+
+        // Activar el correcto
+        if (data.selectedFaceColor == 9) // 👈 color negro (índice 9)
         {
-            eyesAnimator.runtimeAnimatorController = eyesWhiteAnimators[data.selectedEyes];
+            if (data.selectedEyes < eyesWhiteClips.Length)
+            {
+                eyesWhiteAnimator.gameObject.SetActive(true);
+                eyesWhiteAnimator.Play(eyesWhiteClips[data.selectedEyes].name, 0, 0f);
+            }
+            else
+            {
+                Debug.LogError("Índice fuera de rango en eyesWhiteClips");
+            }
         }
         else
         {
-            eyesAnimator.runtimeAnimatorController = eyesAnimators[data.selectedEyes];
+            eyesNormalAnimator.gameObject.SetActive(true);
+            eyesNormalAnimator.Play(eyesClips[data.selectedEyes].name, 0, 0f);
         }
+
+        Debug.Log("Customización aplicada correctamente");
     }
 }
